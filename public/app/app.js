ea7782920379564440418a6bb5bc6909bc4a74a0
Estevão <estevaovasques@hotmail.com>
2018-12-12 22:46:51 -0200

    Limitado a quantidade de chamadas do usuário

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,17 @@
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
+
+const operation = takeUntil(3, () => 
+        service
+            .sumItems('2143')
+            .then(total => console.log(total))
+            .catch(console.error)
+)
 
 
 
 document
     .querySelector('#myButton')
-    .onclick = () =>
-        service
-            .sumItems('2143')
-            .then(total => console.log(total))
-            .catch(console.error)
+    .onclick = operation;
+        
