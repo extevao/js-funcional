fd6db0dc9b6269fb68fcc219c200c41657012abb
Estevão <estevaovasques@hotmail.com>
2018-12-12 22:51:37 -0200

    Adicionado um timeout na request

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,20 @@
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation = takeUntil(3, () => 
+const action = debounceTime(500, 
+    takeUntil(3, () => 
         service
             .sumItems('2143')
             .then(total => console.log(total))
             .catch(console.error)
-)
+    )
+);
+
 
 
 
 document
     .querySelector('#myButton')
-    .onclick = operation;
+    .onclick = action;
         
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -9,3 +9,10 @@ export const pipe = (...fns) => value =>
 export const takeUntil = (times, fn) => 
     () => times-- > 0 && fn();
     
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer);
+        timer = setTimeout(fn, milliseconds);
+    }
+}
