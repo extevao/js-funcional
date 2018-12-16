5f62d7e54f028287f66584038d59b2a742a6608a
Estevão <estevaovasques@hotmail.com>
2018-12-16 20:58:53 -0200

    Retentativa se a request falhar

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { timeoutPromise } from './utils/promise-helpers.js';
+import { timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe, compose } from './utils/operators.js';
@@ -11,7 +11,7 @@ const operations = pipe(
 
 
 const action = operations(() =>
-    timeoutPromise(1000, service.sumItems('2143'))
+    retry(3, 3000, () => timeoutPromise(1000, service.sumItems('2143')))
         .then(total => console.log(total))
         .catch(console.error)
 );
