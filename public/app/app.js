1c14e4793046422a8264ed5dadebc124f3f22d08
Estevão <estevaovasques@hotmail.com>
2018-12-16 21:19:56 -0200

    Criado barramento de eventos para pub/sub

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,7 @@ import { timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe, compose } from './utils/operators.js';
-
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -12,9 +12,9 @@ const operations = pipe(
 
 const action = operations(() =>
     retry(3, 3000, () => timeoutPromise(1000, service.sumItems('2143')))
-        .then(total => console.log(total))
+        .then(total => EventEmitter.emit('itensTotalizados', total))
         .catch(console.error)
-);
+); 
 
 
 document
